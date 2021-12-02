3598ecc6e9622329d74e7b789a98fb5ffba0fcf1
GilHwan <dyghks123@gmail.com>
2021-12-02 19:34:36 +0900

    🚧  Import tiny-slider.js

---
+++ js/productCarousel.js
@@ -0,0 +1,14 @@
+const slider = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  // items: 3,
+  // slideBy: 'page',
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
