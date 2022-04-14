3dc840aa1ba6bf7c2b08c964a452b3404fb78321
GilHwan <dyghks123@gmail.com>
2022-04-14 16:03:07 +0900

    🚧  Seperate user-gallery: mobile and desktop

--- js/productCarousel.js
+++ js/productCarousel.js
@@ -12,11 +12,11 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
 });
 
-const userGallery = tns({
-  container: '.user-gallery .slider-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
   controls: false,
-  controlsContainer: '.user-gallery-controls',
-  navContainer: '.user-gallery .thumbnail-list',
+  controlsContainer: '.user-gallery.is-mobile .user-gallery-controls',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   navAsThumbnails: true,
   gutter: 4,
   loop: false,
@@ -24,11 +24,18 @@ const userGallery = tns({
   arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: true,
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    },
-  },
+});
+
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  navAsThumbnails: true,
+  gutter: 6,
+  loop: false,
+  edgePadding: 75,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
 });
