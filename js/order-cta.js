09dd4044320ab6c03b7cdaaa2d1e83b6d93e5258
GilHwan <dyghks123@gmail.com>
2022-04-20 17:49:04 +0900

    🚧 Implement toggling order-bookmark

--- js/order-cta.js
+++ js/order-cta.js
@@ -18,3 +18,28 @@ function orderClose() {
 
 orderCtaBuyButton.addEventListener('click', orderIsOpen);
 orderModalOverlay.addEventListener('click', orderClose);
+
+function toggleOrderBookmark() {
+  const [icon, countSpan] = this.children;
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''));
+  let newCount = count;
+
+  //   활성화 상태면 -> 비활성화 상태로 전환
+  if (this.classList.contains('is-active')) {
+    icon.classList.add('ic-bookmark');
+    icon.classList.remove('ic-bookmark-filled');
+    newCount = newCount - 1;
+  } else {
+    //   비활성화 상태 -> 활성화 상태로
+    icon.classList.remove('ic-bookmark');
+    icon.classList.add('ic-bookmark-filled');
+    newCount = newCount + 1;
+  }
+  countSpan.innerHTML = newCount.toLocaleString();
+  this.classList.toggle('is-active');
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderBookmark);
+// ic-bookmark-filled
+// is-active
+// count + 1
