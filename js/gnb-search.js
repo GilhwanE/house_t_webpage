2139b6fd1c9c722b7c2c5c0686c4e6a51d9b64cd
GilHwan <dyghks123@gmail.com>
2022-04-22 17:02:21 +0900

    🚧  Implement gnb-search-histroy deleteAll

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,18 @@
 const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbsearchHistory = gnbSearch.querySelector('.search-history');
+const gnbsearchHistoryList = gnbsearchHistory.querySelector('ol');
+const deleteAllButton = gnbSearch.querySelector(
+  '.search-history-header button'
+);
 
 function openGnbSearchHistory() {
+  // check => gnbsearchHistoryList안에 li가 몇개있는지?
+  // li 가 0개면 실행
+  if (gnbsearchHistoryList.children.length === 0) {
+    return;
+  }
+
   if (!gnbsearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory);
   }
@@ -16,4 +26,10 @@ function closeGnbSearchHistory(e) {
   }
 }
 
+function deleteAllitem() {
+  gnbsearchHistoryList.innerHTML = '';
+  gnbsearchHistory.classList.remove('is-active');
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+deleteAllButton.addEventListener('click', deleteAllitem);
