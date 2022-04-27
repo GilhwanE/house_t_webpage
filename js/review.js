6ca3b2072ea56258ad304c9109e39890b62781ad
GilHwan <dyghks123@gmail.com>
2022-04-27 18:22:04 +0900

    🚧  Implement toggling review like button

--- js/review.js
+++ js/review.js
@@ -1,4 +1,6 @@
-const reviewLikeButton = document.querySelector('.review-card-footer button');
+const reviewLikeButtons = document.querySelectorAll(
+  '.review-card-footer button'
+);
 
 const HELPFUL = '도움됨';
 const NOT_HELPFUL = '도움이 돼요';
@@ -11,6 +13,7 @@ function toggleReviewLikeButton() {
 
   const isLiked = this.classList.contains('btn-primary'); //버튼의 상태가 도움이됨
   const textElement = this.nextElementSibling;
+  const reviewCardFooter = this.parentNode;
 
   if (isLiked) {
     // 비활성
@@ -25,15 +28,41 @@ function toggleReviewLikeButton() {
     this.prepend(checkIcon); // 이전 요소에 넣기
   }
 
-  // if (textElement) {
-  // } else {
-  //     if(!isLiked)
-  //     // 앞으로는 활성화 될 것 => 1명
-  //     const newTextElement = document.createElement('p')
-  // }
+  if (textElement) {
+    // N명에게 도움 업데이트
+    const countSpan = textElement.querySelector('span');
+    const count = Number(countSpan.innerHTML.replaceAll('.', ''));
+
+    let newCount = count;
+    if (isLiked) {
+      // 비활성화 -1
+      newCount = newCount - 1;
+      if (newCount === 0) {
+        // textElement 삭제
+        textElement.remove();
+      } else {
+        countSpan.innerHTML = newCount.toLocaleString();
+      }
+    } else {
+      // 활성화 + 1
+      newCount = newCount + 1;
+      countSpan.innerHTML = newCount.toLocaleString();
+    }
+  } else {
+    if (!isLiked) {
+      // 비활성화
+      // 앞으로는 활성화 될 것 => 1명
+      const newTextElement = document.createElement('p');
+      newTextElement.innerHTML =
+        '<strong><span>1</span>명</strong>에게 도움이되었습니다.';
+      reviewCardFooter.appendChild(newTextElement);
+    }
+  }
 
   this.classList.toggle('btn-primary');
   this.classList.toggle('btn-outlined');
 }
 
-reviewLikeButton.addEventListener('click', toggleReviewLikeButton);
+reviewLikeButtons.forEach((button) => {
+  button.addEventListener('click', toggleReviewLikeButton);
+});
