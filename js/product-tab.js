29c64d61e6876be7e3a9f5b5b7f48462d53f0dd6
GilHwan <dyghks123@gmail.com>
2022-04-27 19:45:31 +0900

    💫  Scroll to tab-panel move section

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,6 +1,9 @@
 const productTab = document.querySelector('.product-tab');
 const productTabButtonList = productTab.querySelectorAll('button');
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54;
+const TOP_HEADER_MOBILE = 50 + 40 + 40;
+
 let currentAcitveTab = productTab.querySelector('.is-active'); // 현재 is-active된 버튼 요소 담기
 
 function toggleActiveTab() {
@@ -15,6 +18,22 @@ function toggleActiveTab() {
   }
 }
 
+function scrollTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby');
+  const tabPanel = document.querySelector(`#${tabPanelId}`);
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE); // 현재 나의 윈도우 크기를 알 수 있음
+
+  window.scrollBy({
+    // 스크롤 동작 함수
+    top: scrollAmount,
+    behavior: 'smooth',
+  });
+}
+
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab);
+  button.addEventListener('click', scrollTabPanel);
 });
